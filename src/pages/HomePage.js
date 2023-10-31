6c8b09d4e22471d6051c7e791b2400702a6e61ae
macbook <harisgorcevic1000@gmail.com>
2023-10-31 20:18:39 +0100

    Rad

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./homepage.css";
+import axios from "axios";
 function HomePage() {
+  const [data, setData] = useState();
+  function getData() {
+    axios
+      .get(
+        `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=d31d7724ad474edfbb72f926141ae5e8`
+      )
+      .then((response) => setData(response));
+  }
+  useEffect(() => {
+    getData();
+  }, []);
+  console.log(data);
   return (
     <div className="homepage">
       <div className="banner">
         <img src="gazafree.jpg"></img>
       </div>
+      <div className="main"></div>
     </div>
   );
 }
