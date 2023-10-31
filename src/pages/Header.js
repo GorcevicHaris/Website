f50b2469e18e64b627207cd3d2223e54f65c8c70
macbook <harisgorcevic1000@gmail.com>
2023-10-31 14:35:55 +0100

    RaD

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,7 +1,18 @@
 import React from "react";
 import "./header.css";
-function header() {
-  return <div className="header">header</div>;
+import { useNavigate } from "react-router-dom";
+function Header() {
+  const navigate = useNavigate();
+  return (
+    <div className="header">
+      <div className="logo">
+        <img src="palestine.png"></img>
+      </div>
+      <div className="aboutUs">
+        <h1>About</h1>
+      </div>
+    </div>
+  );
 }
 
-export default header;
+export default Header;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,13 @@
 import React from "react";
 import "./homepage.css";
 function HomePage() {
-  return <div className="homepage">HomePage</div>;
+  return (
+    <div className="homepage">
+      <div className="banner">
+        <img src="gaza.jpg"></img>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
