8397e39b6393272073ecadf7417cab6338e6f414
macbook <harisgorcevic1000@gmail.com>
2023-10-31 14:57:09 +0100

    Rad

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./header.css";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 function Header() {
   const navigate = useNavigate();
   return (
     <div className="header">
       <div className="logo">
-        <img src="palestine.png"></img>
+        <Link to={"/"}>
+          <img src="palestine.png"></img>
+        </Link>
       </div>
       <div className="aboutUs">
         <h1>About</h1>
